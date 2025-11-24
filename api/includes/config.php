da6f286c6f764bada8ddbc8e6ab15219cc74088c
Administrator <Administrator@DESKTOP-A75P7DR>
2025-11-24 16:47:52 +0900

    d

--- api/includes/config.php
+++ api/includes/config.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-	$server_host 		= 'localhost';
+	$server_host 		= '45.250.222.36';
 	$db_user_name 		= 'root';
 	$db_user_password	= 'wlsxo4451';
 	$db_database		= 'l2jserver';
